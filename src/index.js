e315a7a6d014a8f1c4c2cd45ea45aaeba681de30
AviNash N C <31386636+nashnc@users.noreply.github.com>
2025-02-23 21:10:14 +0530

    23/02/2025 @ 21:10

--- src/index.js
+++ src/index.js
@@ -4,16 +4,18 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import store from "./components/store/store";
 
-import { RouterProvider } from "react-router-dom";
+import { BrowserRouter, RouterProvider } from "react-router-dom";
 import router from "./router";
 import { Provider } from "react-redux";
 const root = reactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    {/* <AutoLogin> */}
-    <RouterProvider router={router} />
-    {/* </AutoLogin> */}
-  </Provider>
+  <BrowserRouter basename="/class">
+    <Provider store={store}>
+      {/* <AutoLogin> */}
+      <RouterProvider router={router} />
+      {/* </AutoLogin> */}
+    </Provider>
+  </BrowserRouter>
 );
 
 export default reportWebVitals();
